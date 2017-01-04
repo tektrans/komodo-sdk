ff9c882cd362bfcc66914ccbb88db29b8a32e0b5
Adhidarma Hadiwinoto <me@adhisimon.org>
2017-01-04 20:53:36 +0700

    config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const fs = require('fs');
+
+let configFile = "../../config.json";
+
+if (!fs.existsSync(configFile)) {
+    console.trace('Config file not found. Terminating');
+    setImmedate(function() {
+        process.exit(1);
+    });
+}
+
+const config = require(configFile);
+
+module.exports = config;
