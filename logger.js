42050bdd1f99d4458720f8c8b808dd67495fe740
Adhidarma Hadiwinoto <gua@adhisimon.org>
2017-08-19 13:23:51 +0700

    logger

---
+++ logger.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const fs = require('fs');
+const strftime = require('strftime');
+const winston = require('winston');
+require('winston-daily-rotate-file');
+
+var loggerTimestamp = function() {
+    return strftime('%F %T', new Date());
+}
+
+let logger;
+
+const logDirectory = process.cwd() +  '/logs';
+const filenamePrefix = logDirectory + "/log";
+
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+logger = new winston.Logger({
+    transports: [
+        new (winston.transports.Console)({
+            timestamp: function() {
+                    return strftime('%F %T', new Date());
+            },
+            level: 'verbose',
+        }),
+
+        new (winston.transports.DailyRotateFile) ({
+            name: 'log-file-txt',
+            filename: filenamePrefix,
+            timestamp: loggerTimestamp,
+            formatter: function(options) {
+                return options.timestamp()
+                    +' ' + options.level.toUpperCase()
+                    +' ' + (undefined !== options.message ? options.message : '')
+                    + (options.meta && Object.keys(options.meta).length ? '\n\t' + JSON.stringify(options.meta) : '' );
+            },
+            level: 'debug',
+        }),
+    ]
+});
+
+logger.verbose(__filename + ': initialized');
+
+module.exports = logger;
