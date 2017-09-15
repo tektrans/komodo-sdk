b6d243ea6d56063fbe139addef0666045107df74
Adhidarma Hadiwinoto <gua@adhisimon.org>
2017-09-15 17:21:59 +0700

    logger-circular-buffer-web.listen

--- logger-circular-buffer-web.js
+++ logger-circular-buffer-web.js
@@ -6,31 +6,37 @@ const auth = require('basic-auth');
 const config = require('./config');
 const logger = require('./logger');
 
-if (config && config.logger && && config.logger.circular_buffer_http && config.logger.circular_buffer_http.listen_port) {
-    http.createServer(function(req, res) {
-        var credentials = auth(req);
+function listen(options) {
+    if (options && options.port && options.username && options.password) {
 
-        if (!credentials && credentials.name != config.logger.circular_buffer.username && credentials.pass != config.logger.circular_buffer.password) {
-            res.statusCode = 401;
-            res.setHeader('WWW-Authenticate', 'Basic realm="example"');
-            res.end('Access denied');
-        }
-        else {
+        http.createServer(function(req, res) {
+            var credentials = auth(req);
 
-            logger.query({json: true, order: desc}, function(err, results) {
-                if (err) {
-                    res.end('INVALID LOGGER');
-                }
-                else {
-                    res.writeHead(200, { 'Content-Type': 'application/json' });
-                    res.end(JSON.stringify(results));
-                }
-            });
-        }
-    }).listen(config.logger.circular_buffer_http.listen_port);
+            if (!credentials && credentials.name != options.username && credentials.pass != options.password) {
+                res.statusCode = 401;
+                res.setHeader('WWW-Authenticate', 'Basic realm="example"');
+                res.end('Access denied');
+            }
+            else {
 
-    logger.verbose('Logger circular buffer http server listen on port ' + config.logger.circular_buffer_http.listen_port);
+                logger.query({json: true, order: desc}, function(err, results) {
+                    if (err) {
+                        res.end('INVALID LOGGER');
+                    }
+                    else {
+                        res.writeHead(200, { 'Content-Type': 'application/json' });
+                        res.end(JSON.stringify(results));
+                    }
+                });
+            }
+        }).listen(options.port);
+
+        logger.verbose('Logger circular buffer http server listen on port ' + options.port);
+    }
+    else {
+        logger.verbose('Circular buffer logger http server viewer is not configured. Please set options.port, options.username, options.password');
+    }
+
 }
-else {
-    logger.verbose('Circular buffer logger http reader server is not configured, please set config.logger.circular_buffer.listen_port, config.logger.circular_buffer.username, and config.logger.circular_buffer.password')
-}
+
+exports.listen = listen;
