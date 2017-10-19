5e3b9cbaa760deb12b92c97a89ed42e641fd92ac
Adhidarma Hadiwinoto <gua@adhisimon.org>
2017-10-19 16:50:13 +0700

    simpleGit

--- matrix.js
+++ matrix.js
@@ -1,4 +1,17 @@
 "use strict";
 
+const simpleGit = require('simple-git');
+
 const matrix = {};
+
+// get active git version
+simpleGit(process.cwd()).raw(
+    ['describe'],
+    function(err, result) {
+        if (!err) {
+            matrix.version_active = result;
+        }
+    }
+)
+
 module.exports = matrix;
