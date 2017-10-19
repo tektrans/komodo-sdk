7ca00bf0f21d4e01713274ee6f131c6f6b49b2d7
Adhidarma Hadiwinoto <gua@adhisimon.org>
2017-10-19 18:53:40 +0700

    ubah config-filler.do menjadi config-filler.go

--- config-filler.js
+++ config-filler.js
@@ -3,8 +3,8 @@
 const moment = require('moment');
 const config = require('./config');
 
-function do() {
+function go() {
     moment.locale(config.moment_locale || 'id');
 }
 
-exports.do = do;
+exports.go = go;
