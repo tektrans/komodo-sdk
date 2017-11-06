ff818f63e6ee09a18d0662f692e39032a3100f98
Adhidarma Hadiwinoto <gua@adhisimon.org>
2017-11-06 17:41:59 +0700

    heartbeat

---
+++ gateway/heartbeat.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const request = require('request');
+
+const config = require('../config');
+const logger = require('../logger');
+const matrix = require('../matrix');
+
+function sendHeartbeat() {
+    if (!config || !config.core_url) { return; }
+
+    const requestOptions =  {
+        url: config.core_url + '/heartbeat/gateway'
+        form: {
+            name: config.handler_name,
+            config: JSON.stringify(config),
+            matrix: JSON.stringify(matrix)
+        }
+    }
+
+    request.post(requestOptions);
+}
+
+sendHeartbeat();
+setInterval(
+    sendHeartbeat,
+    60 * 1000
+)
