1803db123e038244d160dd160ec448dc5ea43ef7
Adhidarma Hadiwinoto <gua@adhisimon.org>
2017-11-06 18:19:59 +0700

    module_type as variable

--- heartbeat.js
+++ heartbeat.js
@@ -6,15 +6,17 @@ const config = require('./config');
 const logger = require('./logger');
 const matrix = require('./matrix');
 
+let module_type;
+
 function sendHeartbeat() {
-    if (!config || !config.core_url) { return; }
+    if (!config || !config.core_url || !module_type) { return; }
 
     const requestOptions =  {
         uri: config.core_url + '/heartbeats',
         method: 'POST',
         json: {
             name: config.handler_name,
-            module_type: 'gateway',
+            module_type: module_type,
             config: config,
             matrix: matrix
         }
@@ -28,3 +30,7 @@ setInterval(
     sendHeartbeat,
     60 * 1000
 )
+
+function setModuleType(value) {
+    module_type = value;
+}
