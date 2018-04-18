1bf198d1caae34752e006d2b6b0af95158203150
Adhidarma Hadiwinoto <gua@adhisimon.org>
2018-04-18 13:38:15 +0700

    resendDelay teradaptasi untuk komodo, i hope so

--- gateway/resend-delay.js
+++ gateway/resend-delay.js
@@ -5,63 +5,54 @@ var LRU = require('lru-cache');
 const config = require('./config');
 const logger = require('./logger');
 
-var topupRequest;
 const resendHandlers = LRU({max: 2000, maxAge: 1000 * 3600 * 36});
 
-function init(options) {
-    if (options && options.request) {
-        request = options.request;
-    } else {
-        logger.warn('Undefined options.request, terminating....');
-        process.exit(1);
-    }
-}
-
 function cancel(task) {
-    const requestId = ( typeof task === 'string' ) ? task : task.requestId;
-    if (!requestId) { return; }
+    const trx_id = ( typeof task === 'string' ) ? task : task.trx_id;
+    if (!trx_id) { return; }
 
-    const oldHandler = resendHandlers.get(requestId);
+    const oldHandler = resendHandlers.get(trx_id);
     if (!oldHandler) { return; }
 
-    logger.verbose('Canceling resend delay', {task: oldHandler.task});
+    const task = oldHandler.task;
+    logger.verbose('Canceling resend delay', {trx_id: task.trx_id, destination: task.destination, product: task.product, remote_product: task.remote_product});
+
     if (oldHandler.handler) { clearTimeout(oldHandler.handler); }
-    resendHandlers.del(requestId);
+    resendHandlers.del(trx_id);
 }
 
-function register(task) {
-    if (!task.requestId) {
+function register(task, request) {
+    if (!task.trx_id) {
         logger.warn('Invalid task on resendDelay')
         return;
     }
 
-    if (!config || !config.auto_resend || !Number(config.auto_resend.delay_ms) || !Number(config.auto_resend.max_retry)) {
+    if (!request || !config || !config.auto_resend || !Number(config.auto_resend.delay_ms) || !Number(config.auto_resend.max_retry)) {
         return;
     }
 
-    var retry = config.auto_resend.max_retry;
-    var oldHandler = resendHandlers.get(task.requestId);
+    let retry = config.auto_resend.max_retry;
+    const oldHandler = resendHandlers.get(task.trx_id);
     if (oldHandler) {
         retry = oldHandler.retry - 1;
         cancel(task);
     }
 
     if (retry <= 0) {
-        logger.verbose('Resend delay retry exceeded', {task: task});
+        logger.verbose('Resend delay retry exceeded', {trx_id: task.trx_id, destination: task.destination, product: task.product, remote_product: task.remote_product});
         cancel(task);
         return;
     }
 
-    logger.verbose('Registering resend delay task request', {task: task, delay_ms: config.auto_resend.delay_ms, retry: retry});
-    var handlerData = {
+    logger.verbose('Registering resend delay task request', {trx_id: task.trx_id, destination: task.destination, product: task.product, remote_product: task.remote_product, delay_ms: config.auto_resend.delay_ms, retry: retry});
+    const handlerData = {
         handler: setTimeout(request, config.auto_resend.delay_ms, task),
         task: task,
         retry: retry
     }
 
-    resendHandlers.set(task.requestId, handlerData);
+    resendHandlers.set(task.trx_id, handlerData);
 }
 
-exports.init = init;
 exports.cancel = cancel;
 exports.register = register;
