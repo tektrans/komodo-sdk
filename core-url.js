7712a0586c101ea8638735dd12e3a7990377da86
Adhidarma Hadiwinoto <gua@adhisimon.org>
2018-04-20 21:11:28 +0700

    typo

--- core-url.js
+++ core-url.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const config = require('./config.js');
-const logger = require('../logger');
+const logger = require('./logger');
 const configFromMain = require('./config-from-main');
 
 let core_url;
