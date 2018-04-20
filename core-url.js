965726ade575865503e2d60e56302b2efcc0bb03
Adhidarma Hadiwinoto <gua@adhisimon.org>
2018-04-20 21:22:14 +0700

    debug

--- core-url.js
+++ core-url.js
@@ -7,12 +7,12 @@ const configFromMain = require('./config-from-main');
 let core_url;
 
 if (config.core_url) {
-    logger.verbose('Using CORE url from local config.json');
     core_url = config.core_url;
+    logger.verbose('Using CORE url from local config.json', {url: core_url});
 }
 else if (configFromMain && configFromMain.core && configFromMain.core.url && configFromMain.core.apikey) {
-    logger.verbose('Using CORE url from main config', {filename: configFromMain.this_config_filename});
     core_url = configFromMain.core.url.replace(/\/$/, '') + '/apikey/' + configFromMain.core.apikey + '/pull/task';
+    logger.verbose('Using CORE url from main config', {url: core_url, filename: configFromMain.this_config_filename});
 }
 
 module.exports = core_url;
