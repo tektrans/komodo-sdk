2bc314f2ce5dd1dd5614d876c1b3bb0b55b00af9
Adhidarma Hadiwinoto <gua@adhisimon.org>
2018-04-20 21:10:28 +0700

    smarter core-url

--- config-from-main.js
+++ config-from-main.js
@@ -7,13 +7,19 @@ const candindates = [
     '/etc/komodo/config.js',
     os.homedir() + '/main/config.json',
     process.cwd() + '/../../main/config.json',
+    os.homedir() + '/Projects/tektrans/dev/komodo/config.json'
 ];
 
 let config;
 
 for (let candindate in candindates) {
     if (fs.existsSync(candindate)) {
-        config = require(candindate);
+        try {
+            config = require(candindate);
+            config.this_config_filename = candindate;
+        }
+        catch(e) {}
+
         break;
     }
 }
--- core-url.js
+++ core-url.js
@@ -1,14 +1,17 @@
 "use strict";
 
 const config = require('./config.js');
+const logger = require('../logger');
 const configFromMain = require('./config-from-main');
 
 let core_url;
 
 if (config.core_url) {
+    logger.verbose('Using CORE url from local config.json');
     core_url = config.core_url;
 }
 else if (configFromMain && configFromMain.core && configFromMain.core.url && configFromMain.core.apikey) {
+    logger.verbose('Using CORE url from main config', {filename: configFromMain.this_config_filename});
     core_url = configFromMain.core.url.replace(/\/$/, '') + '/apikey/' + configFromMain.core.apikey + '/pull/task';
 }
 
