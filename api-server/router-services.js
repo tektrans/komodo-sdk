a4e4646bf44293e2559e695750e7657a5ac2d4fd
Adhidarma Hadiwinoto <gua@adhisimon.org>
2018-04-23 16:04:59 +0700

    API-SERVER: services

---
+++ api-server/router-services.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const express = require('express');
+
+const config = require('../config');
+const logger = require('../logger');
+const matrix = require('../matrix');
+
+const router = express.Router();
+module.exports = router;
+
+function isPause(req, res, next) {
+    res.json({
+        method: '/services/is-pause',
+        error: null,
+        result: { isPause: Boolean(matrix.pause) }
+    });
+}
+
+function pause(req, res, next) {
+    matrix.pause = true;
+    res.json({
+        method: '/services/pause',
+        error: null,
+        result: { isPause: Boolean(matrix.pause) }
+    });
+}
+
+function resume(req, res, next) {
+    matrix.pause = false;
+    res.json({
+        method: '/services/resume',
+        error: null,
+        result: { isPause: Boolean(matrix.pause) }
+    });
+}
+
+function terminate(req, res, next) {
+    res.json({
+        method: '/services/terminate',
+        error: null,
+        result: {
+            message: 'Going to restart in ' + delay + 'ms'
+        }
+    })
+}
+
+
+router.get('/is-pause', isPause);
+router.get('/pause', pause);
+router.get('/resume', resume);
