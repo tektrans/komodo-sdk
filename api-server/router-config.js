02f4c768459c9b76e3834357863d697e6cefffc5
Adhidarma Hadiwinoto <gua@adhisimon.org>
2018-04-23 14:12:55 +0700

    tambahan kurung

--- api-server/router-config.js
+++ api-server/router-config.js
@@ -15,7 +15,7 @@ function getJsonConfig(req, res, next) {
 }
 
 function getConfigElement(req, res, next) {
-    const key = req && req.params && req.params.key ? req.params.key : 'config';
+    const key = (req && req.params && req.params.key) ? req.params.key : 'config';
 
     console.log('KEY: ' + key);
 
