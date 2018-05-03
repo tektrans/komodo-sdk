65757228e0649a2a80c5566740c5245b3b472ed3
Adhidarma Hadiwinoto <gua@adhisimon.org>
2018-05-03 13:29:20 +0700

    debug

--- rc-from-msg.js
+++ rc-from-msg.js
@@ -1,5 +1,11 @@
 "use strict";
 
+function logOnDebug(msg) {
+    if (process.env.KOMODO_SDK_DEBUG_RC_FROM_MSG) {
+        console.log(msg);
+    }
+}
+
 function run(msg, rules) {
     if (typeof msg !== 'string') return;
     if (!rules) return;
@@ -9,8 +15,10 @@ function run(msg, rules) {
     for(let i = 0; i < rules_count; i++) {
         const rule = rules[i];
 
+        logOnDebug('RC-FROM-MSG: checking with rule: ' + JSON.stringify(rule));
         const re = (typeof rule.flags === 'string') ? new RegExp(rule.pattern, rule.flags) : new RegExp(rule.pattern);
         if (msg.search(re) > 0) {
+            logOnDebug('RC-FROM-MSG: match with rule: ' + JSON.stringify(rule));
             return rule.rc || rule.result;
         }
     }
