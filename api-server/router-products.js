438d7f4c16caef59e59cfa03dff7de6ee888b022
Adhidarma Hadiwinoto <me@adhisimon.org>
2018-06-26 20:26:51 +0700

    simplifikasi products add/del

--- api-server/router-products.js
+++ api-server/router-products.js
@@ -21,7 +21,7 @@ function pageIndex(req, res, next) {
 
 function pageAdd(req, res, next) {
     //if (!req.params.product || req.params.product !== 'string' || !req.params.product.trim()) {
-    if (!req.params.product || req.params.product !== 'string') {
+    if (!req.params.product || !req.params.product.trim()) {
         res.json({
             method: '/products/add',
             error: true,
@@ -46,7 +46,7 @@ function pageAdd(req, res, next) {
 }
 
 function pageDel(req, res, next) {
-    if (!req.params.product || req.params.product !== 'string' || !req.params.product.trim()) {
+    if (!req.params.product || !req.params.product.trim()) {
         res.json({
             method: '/products/del',
             error: true,
