5201c4bb4709ab98a26ffb19c5e6a73bdc65b0f4
Adhidarma Hadiwinoto <me@adhisimon.org>
2018-06-28 22:35:07 +0700

    GW pulltask include cp url

--- gateway/pull.js
+++ gateway/pull.js
@@ -84,7 +84,8 @@ function pullTask() {
             handler: config.handler_name,
             products: config.products.join(','),
             advice_url: (config && config.push_server && config.push_server.apikey && config.push_server.advice && config.push_server.advice.url && config.push_server.advice.port) ? config.push_server.advice.url : null,
-            api_url: (config && config.apiserver && config.apiserver.apikey && config.apiserver.url) ? config.apiserver.url : null
+            api_url: (config && config.apiserver && config.apiserver.apikey && config.apiserver.url) ? config.apiserver.url : null,
+            cp_url: (config && config.control_panel && config.control_panel.url) ? config.control_panel.url : null,
         }
     }
 
