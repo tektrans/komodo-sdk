bbf11a4863e6b9cba67911aa23f829157d4723ba
Adhidarma Hadiwinoto <me@adhisimon.org>
2019-08-02 17:09:24 +0700

    COUNTERS: redis failsafe

--- counters.js
+++ counters.js
@@ -3,26 +3,31 @@
 const redis = require('redis');
 const config = require('./config');
 
-const redisClient = redis.createClient(config.redis || { host: '127.0.0.1' });
+const redisClient = config.redis && redis.createClient(config.redis);
 
 function composeKeyword(name) {
     return `CHONGLEE_COUNTER_${name}}`;
 }
 
 exports.increment = (name) => {
-    redisClient.INCR(composeKeyword(name));
+    redisClient && redisClient.INCR(composeKeyword(name), () => {});
 }
 
 exports.reset = (name) => {
-    redisClient.DEL(composeKeyword(name));
+    redisClient && redisClient.DEL(composeKeyword(name), () => {});
 }
 
 exports.set = (name, value) => {
-    redisClient.SET(composeKeyword(name), Number(value));
+    redisClient && redisClient.SET(composeKeyword(name), Number(value), () => {});
 }
 
 exports.get = (name) => {
     return new Promise((resolve) => {
+        if (!redisClient) {
+            resolve(0);
+            return;
+        }
+
         redisClient.GET(composeKeyword(name), (err, reply) => {
             if (err) {
                 resolve(0);
