1b7eee78c2701524c340b2b9c1d2567bc9ffdffe
Adhidarma Hadiwinoto <me@adhisimon.org>
2019-08-09 12:07:33 +0700

    Logger level verbose

--- logger.js
+++ logger.js
@@ -8,40 +8,33 @@ require('winston-circular-buffer');
 const logDirectory = process.cwd() +  '/logs';
 const filenamePrefix = (global.KOMODO_LOG_FILENAME || process.env.KOMODO_LOG_FILENAME || 'log') + '.';
 
-// const LOG_LABEL = process.env.KOMODO_LOG_LABEL;
-
-// const processTitle = process.title;
-
 const logger = winston.createLogger({
     // levels: winston.config.syslog.levels,
     transports: [
-        /*
-        new (winston.transports.Console)({
-            timestamp: process.stdout.isTTY ? moment() : null,
-            level: 'verbose',
-        }),
-        */
-
         new (winston.transports.Console) ({
+            level: 'verbose',
             format: winston.format.combine(
                 winston.format.metadata(),
                 winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss.SSS' }),
                 winston.format.label({ label: global.KOMODO_LOG_LABEL, message: false }),
-                winston.format.printf((info) => `${process.stdout.isTTY ? info.timestamp : ''}${info.label ? ' ' + info.label + ':' : ''} ${info.level}: ${info.message} ${info.metadata && Object.keys(info.metadata).length ? JSON.stringify(info.metadata) : ''}`.trim()),
-           )
+                winston.format.printf(
+                    (info) => 
+                    `${process.stdout.isTTY ? info.timestamp : ''}${info.label ? ' ' + info.label + ':' : ''} ${info.level}: ${info.message} ${info.metadata && Object.keys(info.metadata).length ? JSON.stringify(info.metadata) : ''}`.trim()
+                ),
+           ),
         }),
 
         new (winston.transports.DailyRotateFile) ({
+            level: 'verbose',
             filename: `${filenamePrefix}%DATE%`,
             dirname: logDirectory,
             datePattern: 'YYYY-MM-DD',
-            
             format: winston.format.combine(
                 winston.format.metadata(),
                 winston.format.label({ label: global.KOMODO_LOG_LABEL || 'no-label', message: false }),
                 winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss.SSS' }),
                 winston.format.json(),
-            )
+            ),
         }),
 
         /*
