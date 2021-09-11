976a6c5f7f6f08961864ea2c0976522cea6a0a7b
Adhidarma Hadiwinoto <adhisimon@gmail.com>
2021-09-11 17:06:57 +0700

    Start migrating to tektrans-logger

--- logger.js
+++ logger.js
@@ -1,52 +1,9 @@
-const winston = require('winston');
+// const winston = require('winston');
 
-require('winston-daily-rotate-file');
-require('winston-circular-buffer');
+// require('winston-circular-buffer');
 
-const logDirectory = process.cwd() +  '/logs';
-const filenamePrefix = (global.KOMODO_LOG_FILENAME || process.env.KOMODO_LOG_FILENAME || 'log') + '.';
+const logger = require('tektrans-logger');
 
-const logger = winston.createLogger({
-    // levels: winston.config.syslog.levels,
-    transports: [
-        new (winston.transports.Console) ({
-            level: 'verbose',
-            format: winston.format.combine(
-                winston.format.metadata(),
-                winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss.SSS' }),
-                winston.format.label({ label: global.KOMODO_LOG_LABEL, message: false }),
-                winston.format.printf(
-                    (info) => 
-                    `${process.stdout.isTTY ? info.timestamp : ''}${info.label ? ' ' + info.label + ':' : ''} ${info.level}: ${info.message} ${info.metadata && Object.keys(info.metadata).length ? JSON.stringify(info.metadata) : ''}`.trim()
-                ),
-           ),
-        }),
-
-        new (winston.transports.DailyRotateFile) ({
-            level: 'verbose',
-            filename: `${filenamePrefix}%DATE%`,
-            dirname: logDirectory,
-            datePattern: 'YYYY-MM-DD',
-            format: winston.format.combine(
-                winston.format.metadata(),
-                winston.format.label({ label: global.KOMODO_LOG_LABEL || 'no-label', message: false }),
-                winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss.SSS' }),
-                winston.format.json(),
-            ),
-        }),
-
-        /*
-        new (winston.transports.CircularBuffer) ({
-            name: 'logs',
-            level: "verbose",
-            json: true,
-            size: 500
-        }),
-        */
-    ]
-});
-
-logger.info('Logger initialized');
 require('./logger-circular-buffer-web');
 
 module.exports = logger;
