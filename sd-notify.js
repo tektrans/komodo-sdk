ec599ebefe403fe7fec191fc86c1aa234c5b08fe
Adhidarma Hadiwinoto <adhisimon@gmail.com>
2022-03-15 16:32:38 +0700

    Fix sd-notify wrapper

--- sd-notify.js
+++ sd-notify.js
@@ -5,10 +5,12 @@ const MODULE_NAME = 'KOMODO-SDK.SD-NOTIFY';
 
 const logger = require('tektrans-logger');
 
-try {
-    const notify = require('sd-notify');
-    notify.ready();
-    logger.info(`${MODULE_NAME} 701F8400: Systemd ready notification has been sent`);
-} catch (e) {
-    logger.warn(`${MODULE_NAME} A6C99938: Optional dependency not found: sd-notify`);
-}
+module.exports = () => {
+    try {
+        const notify = require('sd-notify');
+        notify.ready();
+        logger.info(`${MODULE_NAME} 701F8400: Systemd ready notification has been sent`);
+    } catch (e) {
+        logger.warn(`${MODULE_NAME} A6C99938: Optional dependency not found: sd-notify`);
+    }
+};
