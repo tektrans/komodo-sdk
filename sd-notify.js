e164a6fb4b70d3ac07242a271210942c3baf0eb2
Adhidarma Hadiwinoto <adhisimon@gmail.com>
2022-03-15 16:31:23 +0700

    Add sd-notify wrapper

---
+++ sd-notify.js
@@ -0,0 +1,14 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-extraneous-dependencies */
+
+const MODULE_NAME = 'KOMODO-SDK.SD-NOTIFY';
+
+const logger = require('tektrans-logger');
+
+try {
+    const notify = require('sd-notify');
+    notify.ready();
+    logger.info(`${MODULE_NAME} 701F8400: Systemd ready notification has been sent`);
+} catch (e) {
+    logger.warn(`${MODULE_NAME} A6C99938: Optional dependency not found: sd-notify`);
+}
