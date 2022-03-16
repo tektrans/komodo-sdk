7616bf2596787222ad693968c61a6889f5813f97
Adhidarma Hadiwinoto <adhisimon@gmail.com>
2022-03-16 12:04:11 +0700

    Touch matrix.systemd_notified on invoking sd-notify

--- sd-notify.js
+++ sd-notify.js
@@ -4,11 +4,15 @@
 const MODULE_NAME = 'KOMODO-SDK.SD-NOTIFY';
 
 const logger = require('tektrans-logger');
+const matrix = require('./matrix');
 
 module.exports = () => {
     try {
         const notify = require('sd-notify');
+
         notify.ready();
+        matrix.systemd_notified = new Date();
+
         logger.info(`${MODULE_NAME} 701F8400: Systemd ready notification has been sent`);
     } catch (e) {
         logger.warn(`${MODULE_NAME} A6C99938: Optional dependency not found: sd-notify`);
