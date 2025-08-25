bd290f7c51f7c386d6844b40a7956528129ae923
Adhidarma Hadiwinoto <me@adhisimon.org>
2025-08-25 17:52:05 +0700

    MODULE_NAME on gateway.advice-push-server

--- gateway/advice-push-server.js
+++ gateway/advice-push-server.js
@@ -1,3 +1,5 @@
+const MODULE_NAME = 'KOMODO-SDK.GATEWAY.ADVICE-PUSH-SERVER';
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('tektrans-logger');
@@ -12,47 +14,57 @@ let partner = null;
 function setPartner(_partner) {
     partner = _partner;
 
-    if (!config || !config.push_server || !config.push_server.apikey || !config.push_server.advice || !config.push_server.advice.url || !config.push_server.advice.port) {
+    if (
+        !config
+        || !config.push_server
+        || !config.push_server.apikey
+        || !config.push_server.advice
+        || !config.push_server.advice.url
+        || !config.push_server.advice.port) {
         return;
     }
 
-    app.listen(config.push_server.advice.port, function () {
-        logger.info('Advice server listening', {port: config.push_server.advice.port});
+    app.listen(config.push_server.advice.port, () => {
+        logger.info(`${MODULE_NAME} 7C994547: Listening`, {
+            port: config.push_server.advice.port,
+        });
     });
 }
 
 function isValidApikey(req, res, next) {
-    if (config.push_server && config.push_server.apikey && (req.params.apikey === config.push_server.apikey)) {
+    if (
+        config.push_server
+        && config.push_server.apikey
+        && (req.params.apikey === config.push_server.apikey)
+    ) {
         next();
-    }
-    else {
+    } else {
         res.end('INVALID_APIKEY');
     }
 }
 
 function adviceHandler(req, res) {
-
     if (!partner) {
-        logger.warn('PUSH-ADVICE: Undefined partner, skipped');
+        logger.warn(`${MODULE_NAME} 58FACCD9: Undefined partner, skipped`);
         res.end('UNDEFINED_PARTNER');
         return;
     }
 
     if (!partner.advice) {
-        logger.warn('PUSH-ADVICE: Partner does not have ADVICE capabilities');
+        logger.warn(`${MODULE_NAME} 73E745A0: Partner does not have ADVICE capabilities`);
         res.end('UNSUPPORTED');
         return;
     }
 
-    let task = req.body;
+    const task = req.body;
 
     if (!task || !task.trx_id || !task.destination || !task.product) {
-        logger.warn('PUSH-ADVICE: Invalid task');
+        logger.warn(`${MODULE_NAME} DABD50A5: Invalid task`);
         res.end('INVALID_TASK');
         return;
     }
 
-    logger.verbose('PUSH-ADVICE: Got advice push', {task: task});
+    logger.verbose(`${MODULE_NAME} DB27D06B: Got advice push`, { task });
 
     task.remote_product = pull.getRemoteProduct(task.product);
     if (Number(config.sdk_trx_id_adder)) {
