07d0b3c40b5ebbc60a443bbcc00654cd733928c2
Adhidarma Hadiwinoto <adhisimon@tektrans.id>
2026-09-25 17:38:04 +0700

    chore(advice-push-server): fix advice capability detection

--- gateway/advice-push-server.js
+++ gateway/advice-push-server.js
@@ -24,8 +24,8 @@ function setPartner(_partner) {
         return;
     }
 
-    if (!partner.advice || typeof partner !== 'function') {
-        logger.info(`${MODULE_NAME} 1E342CCE: No advice funtion detected, skip listening`);
+    if (!partner.advice || typeof partner.advice !== 'function') {
+        logger.info(`${MODULE_NAME} 1E342CCE: No advice funcction detected, skip listening`);
         return;
     }
 
